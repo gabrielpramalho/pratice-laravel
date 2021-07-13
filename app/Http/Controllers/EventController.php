418e65f65c787954bbe321397368fd4e9fd441b3
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2021-07-13 16:54:07 -0300

    first commit

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Event;
+
+class EventController extends Controller
+{
+    public function index(){
+
+        $search = request('search');
+
+        if($search){
+
+            $events = Event::where([
+                ['tittle', 'like', '%'.$search.'%']
+            ])->get();
+
+        }else{
+            $events = Event::all();
+        }
+
+
+        return view ('welcome', ['events' => $events, 'search'=>$search]);
+    }
+
+    public function create(){
+        return view('events.create');
+    }
+
+    public function store(Request $request){
+
+        $event = new Event;
+
+        $event->tittle = $request->tittle;
+        $event->date = $request->date;
+        $event->city = $request->city;
+        $event->private = $request->private;
+        $event->description = $request->description;
+        $event->items = $request->items;
+
+        //image upload
+
+        if($request->hasFile('image') && $request->file('image')->isValid()){
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now") . "." . $extension);
+
+            $request->image->move(public_path('img/events'), $imageName);
+
+            $event->image = $imageName;
+        }
+
+        $user = auth()->user();
+        $event->user_id = $user->id;
+
+
+        $event->save();
+
+        return redirect('/')->with('msg', 'Evento criado com sucesso!');
+
+    }
+
+    public function show($id){
+
+        $event = Event::findOrFail($id);
+
+        return view('events.show', ['event' => $event]);
+
+    }
+}
+
